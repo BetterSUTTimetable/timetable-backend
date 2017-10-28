73b3be74d6e6ad7f3f71cb9b2e7054b3b1f99418
Rafał Borowiak <rborowiak@euvic.pl>
2017-10-28 12:20:46 +0200

    Project bootstrap.

---
+++ src/main/kotlin/pl/polsl/timetable/TimetableApplication.kt
@@ -0,0 +1,11 @@
+package pl.polsl.timetable
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TimetableApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TimetableApplication::class.java, *args)
+}
---
+++ src/test/kotlin/pl/polsl/timetable/TimetableApplicationTests.kt
@@ -0,0 +1,16 @@
+package pl.polsl.timetable
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TimetableApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
