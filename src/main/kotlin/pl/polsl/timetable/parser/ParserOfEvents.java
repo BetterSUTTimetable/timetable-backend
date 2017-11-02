933f7828aed422bf344f9d967a7e2c24b4cadb40
SerwusX <32936910+SerwusX@users.noreply.github.com>
2017-11-02 20:28:40 +0100

    Naprawiono działanie parsera, obecnie przechodzi test "ParserTest". Dodatkowo konieczna była poprawa formatu dat odczytywanych z pliku *.ics, ponieważ zapisane są w formacie nieobsługiwanym przez klasę Instant (metoda changeDateFormatToISO).

--- src/main/kotlin/pl/polsl/timetable/parser/ParserOfEvents.java
+++ src/main/kotlin/pl/polsl/timetable/parser/ParserOfEvents.java
@@ -3,7 +3,6 @@ package pl.polsl.timetable.parser;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -12,76 +11,70 @@ import java.util.Objects;
 
 public class ParserOfEvents implements Parser {
 
-    private List<Event> events = null;
+    private List<Event> events = new ArrayList<>();
 
     @NotNull
     @Override
     public List<Event> parse(@NotNull BufferedReader reader) throws IOException {
 
-        //Sprawdzenie ilosci zajec w pliku
+        //Zmienne pomocnicze
         String sCurrentLine;
-        int numberOfClasses = 0;
-        try {
-            while((sCurrentLine = reader.readLine()) != null){
-                if (Objects.equals(sCurrentLine, "BEGIN:VEVENT")){
-                    numberOfClasses++;
-                }
-        }
-        } catch (IOException e1) {
-            //niech ginie
-        }
-
-        //Odczytywanie zajec z z pliku do odpowiednich list
-        Long uid;
-        Instant start;
-        Instant end;
-        String summary;
-
-        List<Long> uidList= new ArrayList<>();
-        List<Instant> startList= new ArrayList<>();
-        List<Instant> endList= new ArrayList<>();
-        List<String> summaryList= new ArrayList<>();
+        Long uid = null;
+        Instant start = null;
+        Instant end = null;
+        String summary = null;
 
         try {
             while((sCurrentLine = reader.readLine()) != null){
                 if (sCurrentLine.contains("UID:")){
                     String[] parts = sCurrentLine.split(":");
                     uid = Long.parseLong(parts[1]);
-                    uidList.add(uid);
                 }
                 if (sCurrentLine.contains("DTSTART:")){
                     String[] parts = sCurrentLine.split(":");
-                    start = Instant.parse(parts[1]);
-                    startList.add(start);
+                    start = changeDateFormatToISO(parts[1]);
                 }
                 if (sCurrentLine.contains("DTEND:")){
                     String[] parts = sCurrentLine.split(":");
-                    end = Instant.parse(parts[1]);
-                    endList.add(end);
+                    //end = Instant.parse(parts[1]);
+                    end = changeDateFormatToISO(parts[1]);
                 }
                 if (sCurrentLine.contains("SUMMARY:")){
                     String[] parts = sCurrentLine.split(":");
                     summary = parts[1];
-                    summaryList.add(summary);
+                }
+                if (sCurrentLine.contains("END:VEVENT")){
+                    if (uid == null)
+                        uid = 0L;
+                    if (start == null)
+                        start = Instant.parse("20000101T063000Z");
+                    if (end == null)
+                        end = Instant.parse("20000101T063000Z");
+                    if (summary == null)
+                        summary = "";
+                    events.add(new ParsedEvent(uid, start, end, summary));
+                    uid = null;
+                    start = null;
+                    end = null;
+                    summary = null;
                 }
             }
-        } catch (IOException e1) {
-            //niech ginie
-        }
-
-        //Dodanie zajec do glownej listy
-        for (int i = 0; i < numberOfClasses; i++){
-            Event tmp = new ParsedEvent(
-                    uidList.get(i),
-                    startList.get(i),
-                    endList.get(i),
-                    summaryList.get(i)
-            );
-
-            events.add(tmp);
-        }
-
+        } catch (IOException e1) {}
 
         return events;
+    }
+
+    //Funkcja poprawiajaca format dat w plikach .*ics na ISO
+    private Instant changeDateFormatToISO(String s){
+        String tmp = s;
+
+        tmp = new StringBuilder(tmp).insert(4, "-").toString();
+        tmp = new StringBuilder(tmp).insert(7, "-").toString();
+        tmp = new StringBuilder(tmp).insert(13, ":").toString();
+        tmp = new StringBuilder(tmp).insert(16, ":").toString();
+        tmp = new StringBuilder(tmp).insert(19, ".").toString();
+        tmp = new StringBuilder(tmp).insert(tmp.length()-1, "00").toString();
+
+        return Instant.parse(tmp);
     }
 }
