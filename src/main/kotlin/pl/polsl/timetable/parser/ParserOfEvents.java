483f58e1476c4e6e32c1d4aada82651b30f2e960
Rafał Borowiak <rborowiak@euvic.pl>
2017-11-03 20:06:03 +0100

    Made ICS parser less error-prone.

--- src/main/kotlin/pl/polsl/timetable/parser/ParserOfEvents.java
+++ src/main/kotlin/pl/polsl/timetable/parser/ParserOfEvents.java
@@ -14,50 +14,61 @@ public class ParserOfEvents implements Parser {
     @Override
     public List<Event> parse(@NotNull BufferedReader reader) throws IOException {
 
-        List<Event> events = new ArrayList<>();
+        final List<Event> events = new ArrayList<>();
+        String currentLine;
 
-        //Zmienne pomocnicze
-        String sCurrentLine;
+        while((currentLine = reader.readLine()) != null) {
+            if ("BEGIN:VEVENT".equals(currentLine)) {
+                Event event = parseSingleSection(reader);
+                if (event != null) {
+                    events.add(event);
+                }
+            }
+        }
+
+        return events;
+    }
+
+    private Event parseSingleSection(@NotNull BufferedReader reader) throws IOException {
+        String currentLine;
         Long uid = null;
         Instant start = null;
         Instant end = null;
         String summary = null;
 
-            while((sCurrentLine = reader.readLine()) != null){
-                if (sCurrentLine.contains("UID:")){
-                    String[] parts = sCurrentLine.split(":", 2);
-                    uid = Long.parseLong(parts[1]);
-                }
-                if (sCurrentLine.contains("DTSTART:")){
-                    String[] parts = sCurrentLine.split(":", 2);
-                    start = parseDateFromFileInISOFormat(parts[1]);
-                }
-                if (sCurrentLine.contains("DTEND:")){
-                    String[] parts = sCurrentLine.split(":", 2);
-                    end = parseDateFromFileInISOFormat(parts[1]);
-                }
-                if (sCurrentLine.contains("SUMMARY:")){
-                    String[] parts = sCurrentLine.split(":",2);
-                    if(parts[1].length() == 0)
-                        summary = null;
-                    else
-                        summary = parts[1];
-                }
-                if (sCurrentLine.contains("END:VEVENT")){
-                    if (uid != null && start != null && end != null && summary != null)
-                        events.add(new ParsedEvent(uid, start, end, summary));
-                    uid = null;
-                    start = null;
-                    end = null;
-                    summary = null;
+        while ((currentLine = reader.readLine()) != null) {
+
+            final String[] lineParts = currentLine.split(":", 2);
+
+            if (lineParts.length > 1) {
+                final String keyword = lineParts[0];
+                final String value = lineParts[1];
+
+                if ("UID".equals(keyword)) {
+                    uid = Long.parseLong(value);
+                } else if ("DTSTART".equals(keyword)) {
+                    start = parseToDate(value);
+                } else if ("DTEND".equals(keyword)) {
+                    end = parseToDate(value);
+                } else if ("SUMMARY".equals(keyword)) {
+                    if (value.length() > 0) {
+                        summary = value;
+                    }
+                } else if ("END:VEVENT".equals(currentLine)) {
+                    if (uid != null && start != null && end != null && summary != null) {
+                        return new ParsedEvent(uid, start, end, summary);
+                    } else {
+                        return null;
+                    }
                 }
             }
+        }
 
-        return events;
+        return null;
     }
 
     //Funkcja parsujaca date w formacie ISO z pliku .*ics
-    private Instant parseDateFromFileInISOFormat(String s){
+    private Instant parseToDate(String s){
         s = new StringBuilder(s)
                 .insert(s.length()-1, "00")
                 .insert(4, "-")
